a0a77328379ea26ef1e811715f78838168832632
zhangsongbo <zhangp365@gmail.com>
2025-09-25 17:19:17 +0800

    add logs in depth_anything_v2

--- node_wrappers/depth_anything_v2.py
+++ node_wrappers/depth_anything_v2.py
@@ -1,6 +1,9 @@
 from ..utils import common_annotator_call, INPUT, define_preprocessor_inputs
 import comfy.model_management as model_management
-
+from custom_controlnet_aux.depth_anything_v2 import DepthAnythingV2Detector
+import time
+import logging
+logger = logging.getLogger(__file__)
 class Depth_Anything_V2_Preprocessor:
     loaded_models = {}
 
@@ -23,16 +26,17 @@ class Depth_Anything_V2_Preprocessor:
     CATEGORY = "ControlNet Preprocessors/Normal and Depth Estimators"
 
     def execute(self, image, ckpt_name, resolution=512, **kwargs):
-        from custom_controlnet_aux.depth_anything_v2 import DepthAnythingV2Detector
-        
+        logger.info(f"Depth Anything V2 Preprocessor execute, ckpt_name: {ckpt_name}")
+        start_time = time.time()
         if ckpt_name != self.ckpt_name:
             self.ckpt_name = ckpt_name
             if ckpt_name not in self.loaded_models:
                 self.loaded_models[ckpt_name] = DepthAnythingV2Detector.from_pretrained(filename=ckpt_name).to(model_management.get_torch_device())
         
         model = self.loaded_models[ckpt_name]
+        logger.info(f"Depth Anything V2 Preprocessor model loaded cost: {time.time() - start_time}")
         out = common_annotator_call(model, image, resolution=resolution, max_depth=1)
-
+        logger.info(f"Depth Anything V2 Preprocessor execute, cost: {time.time() - start_time}")
         return (out, )
 
 """ class Depth_Anything_Metric_V2_Preprocessor:
